f98faa80c69deb269fb439db45a0331d0ea75588
Michael <michaelandhsm2@gmail.com>
2018-05-21 14:55:28 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Design Pattern Term Project
+
+This is a ......
+To be written at a later date XD
+
+The project is inspired by [Schuchert’s Monopoly Kata](https://schuchert.wikispaces.com/Monopoly%28r%29).
+
+## Problem Statement
+
+Monopoly is a board game where players roll two six-sided dice to move around the game board, buying and trading properties, and develop them with houses and hotels. Players collect rent from their opponents, with the goal being to drive them into bankruptcy. Money can also be gained or lost through Chance and Community Chest cards, and tax squares; players can end up in jail, which they cannot move from until they have met one of several conditions.
+
+Implement the game with a Test-Driven Approach. With the roll of the dice, users should be able to see the graphic interface implement his/her turn by updating the money, position, and status of the player. A simpler version of the rules may be used to decrease the size of the project.
+
+
+## Initial Plan & Goals
+
+### Iteration 1
+* Simple Graphic Interface Implemented
+* Turn/Phase State System
+* Basic Player Movement
+* Taxes, Income, and Simplified Jail
+### Iteration 2
+* Allow Buying Properties
+* Implementing Selected Community Chest and Chance Cards
+* Jail Implementation
+### Optional
+* Selecting Game Boards (with its own Chest and Chance sets)
+* Allow Mortgages
+* Implementing All Community Chest and Chance Cards
+
+
+## Built With
+
+* NodeJS
+* Boardgame.io
+
+## Authors
+
+* **Michael Fu** - *System Design and Code Implementation* - [michaelandhsm2](https://github.com/michaelandhsm2)
+
+## Acknowledgments
---
+++ src/code/App.js
@@ -0,0 +1,78 @@
+import React from 'react';
+
+import { Client } from 'boardgame.io/react';
+import { Game } from 'boardgame.io/core';
+
+const TicTacToe = Game({
+  setup: () => ({ cells: Array(9).fill(null) }),
+
+  moves: {
+    clickCell(G, ctx, id) {
+      let cells = [...G.cells]; // don't mutate original state.
+      cells[id] = ctx.currentPlayer;
+      return { ...G, cells }; // don't mutate original state.
+    },
+  },
+});
+
+
+
+class TicTacToeBoard extends React.Component {
+  onClick(id) {
+    if (this.isActive(id)) {
+      this.props.moves.clickCell(id);
+      this.props.events.endTurn();
+    }
+  }
+
+  isActive(id) {
+    if (!this.props.isActive) return false;
+    if (this.props.G.cells[id] !== null) return false;
+    return true;
+  }
+
+  render() {
+    let winner = '';
+    if (this.props.ctx.gameover !== null) {
+      winner = <div>Winner: {this.props.ctx.gameover}</div>;
+    }
+
+    const cellStyle = {
+      border: '1px solid #555',
+      width: '50px',
+      height: '50px',
+      lineHeight: '50px',
+      textAlign: 'center',
+    };
+
+    let tbody = [];
+    for (let i = 0; i < 3; i++) {
+      let cells = [];
+      for (let j = 0; j < 3; j++) {
+        const id = 3 * i + j;
+        cells.push(
+          <td style={cellStyle} key={id} onClick={() => this.onClick(id)}>
+            {this.props.G.cells[id]}
+          </td>
+        );
+      }
+      tbody.push(<tr key={i}>{cells}</tr>);
+    }
+
+    return (
+      <div>
+        <table id="board">
+          <tbody>{tbody}</tbody>
+        </table>
+        {winner}
+      </div>
+    );
+  }
+}
+
+const App = Client({
+  game: TicTacToe,
+  board: TicTacToeBoard,
+});
+
+export default App;
