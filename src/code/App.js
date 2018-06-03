af13ecd54cec6a0b32cdfac8dfcf6af84b3d216c
Michael <michaelandhsm2@gmail.com>
2018-06-03 10:29:03 +0800

    Removed boardgame.io

--- src/code/App.js
+++ src/code/App.js
@@ -1,12 +1,43 @@
-import { Client } from 'boardgame.io/react';
+import React from 'react';
+import Board from './ui/board';
+import Canvas from './ui/canvas';
 
-import UI from './ui/reactUI';
-import Game from './rule/game';
+class App extends React.Component {
+  constructor(props){
+    super(props);
 
-const App = Client({
-  game: Game,
-  board: UI,
-  numPlayers: 1,
-});
+    this.board = Object.create(Board);
+    this.board.init();
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  componentDidUpdate() {
+    const canvas = this.refs.canvas;
+    const ctx = this.refs.canvas.getContext('2d');
+    Canvas.clear(ctx, canvas);
+    this.board.onDraw(ctx);
+  }
+
+  render() {
+    return <canvas ref="canvas" width="1000" height="1000"> </canvas>;
+  }
+}
+
 
 export default App;
--- src/code/rule/game.js
+++ src/code/rule/game.js
@@ -1,18 +1,15 @@
-import { Game } from 'boardgame.io/core';
-
 let playerSetup = {
   money: 1500,
   properties: null,
   position: 0
 };
 
-const _Game = Game({
-
+const Game = {
   setup: (ctx) => ({
     players: Array(ctx.numPlayers).fill(playerSetup)
   }),
   moves: {},
   flow: {},
-});
+};
 
-export default _Game;
+export default Game;
