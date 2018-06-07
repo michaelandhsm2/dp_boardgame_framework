a488b0fadbf5c3bd69ef9f847542633f9da65933
Michael <michaelandhsm2@gmail.com>
2018-06-07 15:16:36 +0800

    Add stuff

--- src/index.js
+++ src/index.js
@@ -1,23 +1,32 @@
 import Board from './ui/board';
 import Canvas from './ui/canvas';
+import Game from './rule/game';
 
-var canvas = document.createElement('canvas');
-canvas.id = "canvas";
-canvas.width = 1000;
-canvas.height = 1000;
+var board, game;
 
-var body = document.getElementsByTagName("body")[0];
-body.appendChild(canvas);
+(function onCreate(){
 
-var board = Object.create(Board);
-board.init();
+  const canvas = document.createElement('canvas');
+  canvas.id = "canvas";
+  canvas.width = 1000;
+  canvas.height = 1000;
+  document.getElementsByTagName("body")[0].appendChild(canvas);
 
-requestAnimationFrame(onDraw);
+  game = Object.create(Game);
+  game.setup({
+    numPlayers: 2
+  });
+
+  board = Object.create(Board);
+  board.init(game);
+
+  requestAnimationFrame(onDraw);
+})();
 
 function onDraw() {
-  console.log(this)
+  const canvas = document.getElementById("canvas");
   const ctx = canvas.getContext('2d');
   Canvas.clear(ctx, canvas);
   board.onDraw(ctx);
-  requestAnimationFrame(this.onDraw);
+  requestAnimationFrame(onDraw);
 }
---
+++ src/ui/gameBuilder.js
@@ -0,0 +1,15 @@
+import Canvas from './canvas'
+
+const BASIC_WIDTH = 60
+const BASIC_HEIGHT = 110
+const standard = require('./standard.json')
+
+var GameBuilder = {
+  onUpdate: function(ctx, G){
+    // this.gameBoard
+
+
+  }
+}
+
+export default GameBuilder;
